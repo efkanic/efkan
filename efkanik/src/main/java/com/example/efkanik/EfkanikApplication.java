ff23ec7c6320ec4de24f8dd60219e7ea0dec056b
bprlab <bprlab@10.200.144.14>
2019-04-27 14:11:51 +0300

    yeni projem

---
+++ efkanik/src/main/java/com/example/efkanik/EfkanikApplication.java
@@ -0,0 +1,13 @@
+package com.example.efkanik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfkanikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfkanikApplication.class, args);
+	}
+
+}
